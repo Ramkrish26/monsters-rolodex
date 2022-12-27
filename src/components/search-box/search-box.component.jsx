207f4c8cfebe1c9d6874b07a6aee0bb7b35b7138
Ramakrishnan Rajaram <ramakrishnan2629@gmail.com>
2022-12-27 15:37:41 +0530

    Basic Components (#1)
    1. Card list component
    2. Search box component

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class SearchBox extends Component {
+    render() {
+        return (
+            <input className={this.props.className}
+                type='search'
+                placeholder={this.props.placeHolder}
+                onChange={this.props.onSearchChangeHandler} />
+        );
+    }
+}
+
+export default SearchBox;
